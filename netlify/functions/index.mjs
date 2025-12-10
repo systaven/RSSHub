0cd39337051af148cea0c57c8f5caf5979c13941
systaven <stevenyang770406@gmail.com>
2025-12-09 21:53:34 -0500

    Change import from server.mjs to server.ts

--- netlify/functions/index.mjs
+++ netlify/functions/index.mjs
@@ -1,5 +1,5 @@
 import { handle } from 'hono/netlify' // 引入 Hono 的 Netlify 适配器
-import app from '../../src/server.mjs' // 引入 RSSHub 的核心程序
+import app from '../../src/server.ts'
 
 // 把 RSSHub 的 Hono 应用转换成 Netlify Function
 export const handler = handle(app)
